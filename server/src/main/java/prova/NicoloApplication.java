df589e650c0c857e3a1110a81e2ec617a77a39f5
Nicolo' Abatecola <nicoloantoninoabatecola@gmail.com>
2021-05-03 12:21:26 +0200

    Creare server #1

---
+++ server/src/main/java/prova/NicoloApplication.java
@@ -0,0 +1,13 @@
+package prova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicoloApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/prova/NicoloApplicationTests.java
@@ -0,0 +1,13 @@
+package prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
